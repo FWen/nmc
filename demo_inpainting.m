bae0154ac7badca0be4b9ca4ca581c70d74609f0
FWen <wenfei@sjtu.edu.cn>
2019-12-27 11:38:22 +0800

    Add files via upload

--- demo_inpainting.m
+++ demo_inpainting.m
@@ -1,4 +1,4 @@
-clear all; clc; % close all;
+clear all; clc; close all;
 
 WIDTH = 512;
 X = imresize(double(imread('boat.png')),[WIDTH, WIDTH]);
@@ -6,10 +6,8 @@ X = imresize(double(imread('boat.png')),[WIDTH, WIDTH]);
 
 [S,V,D] = svd(X);
 v = (diag(V));
-% figure(2);
-% plot(1:n,v/max(v),'-'); xlim([1,n]);
 
-figure(3);subplot(121);
+figure(1);subplot(121);
 semilogy(1:m,v,'r-','linewidth',1); xlim([1,512]);%ylim([0.1,1e5]);
 ylabel('Singular value');title('No-strictly low-rank');
 legend('No-strictly low-rank');
@@ -18,8 +16,7 @@ legend('No-strictly low-rank');
 r = round(0.15*WIDTH);
 [S,V,D] = svd(X);
 X = S(:,1:r)*V(1:r,1:r)*D(:,1:r)';
-% figure(1);subplot(2,2,2);
-% imshow(uint8(X2));
+
 
 subplot(122);
 semilogy(1:m,v,'b-','linewidth',2); xlim([1,512]);%ylim([0.1,1e5]);
@@ -33,73 +30,35 @@ P = zeros(m*n,1);
 P(J) = 1;
 P = reshape(P,[m,n]);   % Projection Matrix
 
-%----entry-wise noise----------
+% ----entry-wise noise----------
 Y = X(:);
 SNR = 40;
 noise = randn(m*n,1);
 noise = noise/std(noise) *10^(-SNR/20)*std(Y);  
 Y = Y + noise;
 
-% ---partial observation---
+% ---partial observation---------
 Y = reshape(Y,[m,n]).*P; 
 
 
-figure(1); subplot(1,3,1);
+figure(2); subplot(1,3,1);
 imshow(uint8(Y));
 title('Partial observation (50%)');
 
 
 %  L1 --------------------
-lamdas = logspace(-1,7,40);
-parfor k = 1:length(lamdas); 
-%     k=15
-    [Xr, out]   = lq_pgd_mc(1, Y, P, lamdas(k), X, zeros(size(Y)));
-    relerr1(k)  = norm(Xr-X,'fro')/norm(X,'fro');
-    PSNR1(k) = psnr(X, Xr);
-    X_l1(:,:,k) = Xr;
-    
-%   figure();semilogy(1:length(out.e),out.e,'-');
-end
-% figure(4);semilogy(lamdas,relerr1,'r-o');set(gca,'xscale','log');
-
-[RelErr1, mi] = min(relerr1);
-X_L1 = X_l1(:,:,mi); 
+[X_l1, out]  = lq_pgd_mc(1, Y, P, 10, X, zeros(size(Y)));
+relerr = norm(X_l1-X,'fro')/norm(X,'fro');
 
 figure(1); subplot(1,3,2);
-imshow(uint8(X_L1));
-title(sprintf('Soft (RelErr=%.5f, PSNR=%.2f dB)', RelErr1, PSNR1(mi)));
+imshow(uint8(X_l1));
+title(sprintf('Soft-PGD (RelErr=%.5f, PSNR=%.2f dB)', relerr, psnr(X, X_l1)));
+
 
 %  Lq --------------------
-qs = 0:0.1:1;
-for kq=1:length(qs)
-    qs(kq)
-    parfor k=1:length(lamdas)       
-        [Xr,~]       = lq_pgd_mc(qs(kq), Y, P, lamdas(k), X, X_L1);
-        relerr(kq,k) = norm(Xr-X,'fro')/norm(X,'fro');
-        PSNR(kq,k)   = psnr(X, Xr);
-        X_lq(:,:,k)  = Xr;
-    end
-%     figure();semilogy(lamdas,relerr(kq,:),'r-o');set(gca,'xscale','log');
-    
-    [RelErr, mi]  = min(relerr(kq,:));
-    Imgrs(:,:,kq) = X_lq(:,:,mi);
-    RelErrs(kq)   = RelErr;
-    PSNRr(kq)     = PSNR(kq,mi);
-end
+[X_lq,~]    = lq_pgd_mc(0.1, Y, P, 1e3, X, X_l1);
+relerr = norm(X_lq-X,'fro')/norm(X,'fro');
 
-v0 = min(min(PSNR));
-v1 = max(max(PSNR));
-figure(6);
-contourf(qs,lamdas,PSNR',[v0:0.5:v1]); colorbar; ylabel('\lambda'); xlabel('q');
-set(gca, 'CLim', [v0, v1]);set(gca,'yscale','log');
-
-[w1, e1] = max(PSNR'); [~, lo] = max(w1); ko = e1(lo);
-figure(6);hold on;
-plot(qs(lo),lamdas(ko),'r*');hold off;
-
-[min_RelErr, mi] = min(RelErrs);
-
-figure(1);subplot(1,3,3);
-imshow(uint8(Imgrs(:,:,mi)));
-title(['Lq (best q=', num2str(qs(mi), '%.1f'), ', ', 'RelErr=', num2str(min_RelErr, '%.5f'),', ', 'PSNR=', num2str(PSNRr(mi), '%.2f') ' dB)']);
-
+figure(2);subplot(1,3,3);
+imshow(uint8(X_lq));
+title(sprintf('Lq-PGD (RelErr=%.5f, PSNR=%.2f dB)', relerr, psnr(X, X_lq)));
